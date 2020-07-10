22fb705e2846f3142202540e70c9c5ef3cfdc3c0
MiladYarzaman1370 <yarzaman.milad@gmail.com>
2020-07-10 20:39:53 +0330

    intial project

---
+++ src/test/java/com/example/bot/gameBot/GameBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bot.gameBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
